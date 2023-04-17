e65e68600fca6ad06adde5bed8f4d3c80fe28610
Saatvik Rao <saatvikrao@Saatviks-MacBook-Air.local>
2023-04-18 00:28:07 +0530

    changed data

--- to_create_dataset.py
+++ to_create_dataset.py
@@ -8,7 +8,7 @@ user_agent = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 } 
 
-saved_folder = 'yeeshu-saatvik-assignment5/images/rat' 
+saved_folder = 'yeeshu-saatvik-assignment5/images/squirrel' 
 
 
 def main():
